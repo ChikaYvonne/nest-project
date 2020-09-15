93158837c23460522778a4ad04c403a42ae8bd10
Chika Nwogu <piosystems@yahoo.co.uk>
2020-09-15 19:11:18 +0100

    first Commit

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+    mongoURI: 'mongodb://Chika:Chichi08@cluster0-shard-00-00.hopvg.mongodb.net:27017,cluster0-shard-00-01.hopvg.mongodb.net:27017,cluster0-shard-00-02.hopvg.mongodb.net:27017/items?ssl=true&replicaSet=atlas-12kb45-shard-0&authSource=admin&retryWrites=true&w=majority'
+    
+}
---
+++ src/items/items.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CreateItemDto } from './dto//create-item.dto';
+import {ItemsService } from './items.service';
+import { Item} from './interfaces/item.interface'
+
+@Controller('items')
+export class ItemsController { 
+constructor(private readonly itemService: ItemsService) {}
+
+    @Get()
+    findAll(): Promise<Item[]> {
+        return this.itemService.findAll();
+    } 
+
+    @Get(':id')
+    findOne(@Param('id') id): Promise<Item> {
+        return this.itemService.findOne(id);
+    } 
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+        return this.itemService.create(createItemDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id): Promise<Item> {
+        return this.itemService.delete(id);
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+      return this.itemService.update(id, updateItemDto);
+    }
+}
+ 
